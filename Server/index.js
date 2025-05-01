9607fab8c84c5c0557b822e0a1dbfbb673af3ef1
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-01 20:44:08 +0600

    update

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var app = express();
-var port = 5022;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
-});
+const express = require('express')
+const app = express()
+const port = 5022
+
+app.get('/', (req, res) => {
+  res.send('Server is running !')
+})
+
+app.get('/data', (req,res) => {
+  res.send('hello')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
--- Server/index.ts
+++ Server/index.ts
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 5022
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is running !')
+})
+
+app.get('/data', (req,res) => {
+  res.send('<h1>this is data</h1>')
 })
 
 app.listen(port, () => {

2000120630d749b638bc771dfa2b3cd4db16e37b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-06 12:21:41 +0600

    upadge

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,11 @@ import express from "express"
 const app = express()
 const port = 5022
 
+
+
+app.use(express.json());
+
+
 // app.get('/', (req, res) => {
 //   res.send('Server is running !')
 // })
@@ -31,6 +36,34 @@ app.get('/snack', (req,res) =>{
   ]
   res.send(snack)
 })
+
+// post data
+
+app.post('/post-data',async (req,res)=>{
+  const BodyData = req.body;
+  const data = JSON.stringify(BodyData);
+  console.log(`the body data is => ${data}`);
+  try{
+    res.send(data)
+  }catch(error){
+    console.error('Error is send',error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// post data and do something with date
+app.post('/do',async(req,res)=>{
+  const Body = req.body;
+  console.log(`this is body data -> ${Body}`);
+  try{
+    const Text = Body.data;
+    console.log(`This is result -> ${Text}`);
+    res.send(Text)
+  }catch(error){
+    console.error('error is coming for something do',error)
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

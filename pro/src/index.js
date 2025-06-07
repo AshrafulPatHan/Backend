ca0d067d88098b0b711efa49ffe1237f73129d0e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-07 15:43:25 +0600

    i dont know what i do

---
+++ pro/src/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { DB_NAME } from './constants';
+import express from 'express';
+const app = express() 
+
+
+( async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("app is not work and error",error);
+            throw error
+        })
+
+        // listen port
+        app.listen(process.env.PORT,()=>{
+            console.log(`the port is ${process.env.PORT}`);
+            
+        })
+    }catch(error){
+        console.error('error is mongoose',error)
+        throw error
+    }
+})()
+

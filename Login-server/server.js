cffbf6e27890371ab96c2431645410a336c31ac8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-01-16 11:26:23 +0600

    app is start

--- Login-server/server.js
+++ Login-server/server.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 
+// home route
+app.get('/',(req,res)=>{
+    res.send("Server is running version 0.1")
+})
 
+// listening port
+app.listen(PORT,()=>{
+    console.log(`Express app listening on port ${PORT}`);
+    
+})

03dd4ebfa82721eaaf6cd870bde3d794559efdfc
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-01-16 12:05:15 +0600

    render the clinte

--- Login-server/server.js
+++ Login-server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 3000;
 
 
@@ -7,6 +8,19 @@ const PORT = 3000;
 app.get('/',(req,res)=>{
     res.send("Server is running version 0.1")
 })
+
+// render client
+app.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','index.html')); // home
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','login.html')); // login
+});
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','registration.html')); // registration
+});
+
+// app.use(express.static(path.join(__dirname,'client'))); # for rendering full clint 
 
 // listening port
 app.listen(PORT,()=>{

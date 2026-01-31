ecf32310ae4aa6a35899ca0ce04b8883608c5875
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-01-31 12:56:20 +0600

    add some packeg

--- jwt/server.js
+++ jwt/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
 const port = 3000;
 
+app.use(express.json());
+app.use(cors())
+
+// home route
 app.get('/',(req,res)=>{
     res.send('Server is running 0.1');
 });
